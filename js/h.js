998782397e298a71400b7aaee06cbc73e966fa92
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-13 16:28:26 +0000

    Create h.js

---
+++ js/h.js
@@ -0,0 +1,15 @@
+document.write(`
+  <header>
+    <a class="title" href="/">
+      <img src="/images/logos/chrox.png" width="128" height="128">
+      <h2>Chrox</h2>
+    </a>
+    <nav id="nav-1">
+      <a href="https://github.com/CybriaTech/Chrox">Github</a>
+      <a href="https://bit.ly/cybriatech">Discord</a>
+      <a href="https://buymeacoffee.com/cybriatech">Donate</a>
+    <nav class="nav-2">
+      <button onclick="theme()"></button>
+    </nav>
+  </header>
+`);
