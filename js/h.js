6f7a7b8dfbfc7f423e6debdaad156e240284cd7d
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-16 18:50:00 -0600

    Update h.js

--- js/h.js
+++ js/h.js
@@ -1,9 +1,9 @@
 document.write(`
   <header>
-    <a class="title" href="/">
+    <a style="margin: 0 300px;" class="title" href="/">
       <p>Chrox</p>
     </a>
-    <nav>
+    <nav style="margin: 0 300px;">
       <a href="https://github.com/CybriaTech/Chrox">Github</a>
       <a href="https://bit.ly/cybriatech">Discord</a>
       <a href="https://buymeacoffee.com/cybriatech">Donate</a>
