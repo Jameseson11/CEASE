0e6626996130e1672b879e35ecaad29dc199dd4c
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-26 16:40:48 +0000

    Update h.js

--- js/h.js
+++ js/h.js
@@ -3,9 +3,15 @@ document.write(`
   <nav style="z-index: 99999; display: flex; justify-content: flex-start; align-items: center;">
     <a href="/">CEASE</a>
   </nav>
+    <nav style="z-index: 99999; display: flex; justify-content: center; align-items: center;">
+      <a href="/exploits">Exploits</a>
+      <a href="/about.html">About</a>
+      <a href="/settings.html">Settings</a>
+      </nav>
     <nav style="z-index: 99999; display: flex; justify-content: flex-end; align-items: center;">
       <a href="https://github.com/CybriaTech/CEASE"><i class="fab fa-github"></i></a>
       <a href="https://bit.ly/cybriatech"><i class="fab fa-discord"></i></a>
       <a href="https://buymeacoffee.com/cybriatech"><i class="fas fa-heart"></i></a>
+      </nav>
   </header>
 `);
