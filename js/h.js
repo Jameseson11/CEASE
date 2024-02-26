7eadaf5fabff09da1ab1d5bba26a7a8a8ce83ef2
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-26 03:49:53 +0000

    Update h.js

--- js/h.js
+++ js/h.js
@@ -1,10 +1,11 @@
 document.write(`
   <header>
     <a style="margin: 0 300px;" class="title" href="/">
-      <p>Chrox</p>
+      <p>CEASE</p>
+      <img src="/images/logos/cease.png" style="width: 32px; height: 32px;" alt="CEASE">
     </a>
     <nav style="margin: 0 300px;">
-      <a href="https://github.com/CybriaTech/Chrox">Github</a>
+      <a href="https://github.com/CybriaTech/CEASE">Github</a>
       <a href="https://bit.ly/cybriatech">Discord</a>
       <a href="https://buymeacoffee.com/cybriatech">Donate</a>
   </header>
