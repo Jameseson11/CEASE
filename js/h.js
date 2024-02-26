edbc71187378c819619bc598d8f169dd4ceee1b0
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-26 04:08:07 +0000

    Update h.js

--- js/h.js
+++ js/h.js
@@ -1,9 +1,14 @@
 document.write(`
   <header>
-    <a style="margin: 0 300px;" class="title" href="/">
-      <img src="/images/logos/cease.png" style="width: 32px; height: 32px;" alt="CEASE">
-    </a>
-    <nav style="margin: 0 300px;">
+  <nav style="z-index: 99999; display: flex; justify-content: flex-start; align-items: center;">
+    <a href="/">Home</a>
+    <a href="/g/">Games</a>
+    <a href="/p/">Proxy</a>
+    <a href="/e/b.html">Bookmarklets</a>
+    <a href="/e/m.html">Mirrors</a>
+    <a href="/e/s.html">Settings</a>
+  </nav>
+    <nav style="z-index: 99999; display: flex; justify-content: flex-end; align-items: center;">
       <a href="https://github.com/CybriaTech/CEASE">Github</a>
       <a href="https://bit.ly/cybriatech">Discord</a>
       <a href="https://buymeacoffee.com/cybriatech">Donate</a>
