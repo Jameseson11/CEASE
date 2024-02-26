262e5142e10ded759e27c0671bb579729594479c
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-26 04:00:59 +0000

    Update h.js

--- js/h.js
+++ js/h.js
@@ -1,6 +1,6 @@
 document.write(`
   <header>
-    <a style="margin: 0 300px;" class="title" href="/">
+    <a style="margin: 300px;" class="title" href="/">
       <img src="/images/logos/cease.png" style="width: 32px; height: 32px;" alt="CEASE">
     </a>
     <nav style="margin: 0 300px;">
