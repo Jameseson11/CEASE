202923d329f20195cd3f20eef6c539d8dde9dace
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-29 16:16:19 -0600

    Update h.js

--- js/h.js
+++ js/h.js
@@ -1,17 +1,18 @@
 document.write(`
-  <header>
+<header>
   <nav style="z-index: 99999; display: flex; justify-content: flex-start; align-items: center;">
     <a href="/">CEASE</a>
   </nav>
-    <nav style="z-index: 99999; display: flex; justify-content: center; align-items: center;">
-      <a href="/docs">Docs</a>
-      <a href="/about.html">About</a>
-      <a href="/settings.html">Settings</a>
-      </nav>
-    <nav style="z-index: 99999; display: flex; justify-content: flex-end; align-items: center;">
-      <a href="https://github.com/CybriaTech/CEASE"><i class="fab fa-github"></i></a>
-      <a href="https://bit.ly/cybriatech"><i class="fab fa-discord"></i></a>
-      <a href="https://buymeacoffee.com/cybriatech"><i class="fas fa-heart"></i></a>
-      </nav>
-  </header>
+  <nav style="z-index: 99999; display: flex; justify-content: center; align-items: center; margin: 0 auto;">
+    <a href="/docs">Docs</a>
+    <a href="/about.html">About</a>
+    <a href="/settings.html">Settings</a>
+  </nav>
+  <nav style="z-index: 99999; display: flex; justify-content: flex-end; align-items: center;">
+    <a href="https://github.com/CybriaTech/CEASE"><i class="fab fa-github"></i></a>
+    <a href="https://bit.ly/cybriatech"><i class="fab fa-discord"></i></a>
+    <a href="https://buymeacoffee.com/cybriatech"><i class="fas fa-heart"></i></a>
+  </nav>
+</header>
+
 `);
