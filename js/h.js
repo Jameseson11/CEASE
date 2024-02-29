909ad9447d66e27d7d3ffcc120ec9419b8246960
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-29 21:10:02 +0000

    Update h.js

--- js/h.js
+++ js/h.js
@@ -5,7 +5,7 @@ document.write(`
   </nav>
     <nav style="z-index: 99999; display: flex; justify-content: center; align-items: center;">
       <a href="/docs">Docs</a>
-      <a href="/mirror.html">Mirrors</a>
+      <a href="/about.html">About</a>
       <a href="/settings.html">Settings</a>
       </nav>
     <nav style="z-index: 99999; display: flex; justify-content: flex-end; align-items: center;">
