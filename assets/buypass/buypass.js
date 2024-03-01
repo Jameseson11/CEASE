3ce529e44253e3651213a290e4ae6484d9dedd29
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-03-01 16:35:57 +0000

    Update buypass.js

--- assets/buypass/buypass.js
+++ assets/buypass/buypass.js
@@ -1,16 +1,18 @@
-function buypass() {//Credit to Bypassi#7037 (integrated function)
+function buypass() {
     new PaymentRequest(
         [
             {
-                supportedMethods: "/pay/main.json",
-                data: {url: "https://google.com"},
+                supportedMethods: location.origin + "/pay/main.json",
+                data: { url: "https://google.com"},
             },
         ],
         {
             total: {
                 label: "_",
-                amount: {value: "1", currency: "USD"},
-            }
+                amount: { value: "1", currency: "USD" },
+            },
         }
     ).show();
 }
+
+document.querySelector("script-btn").onclick = buypass;
